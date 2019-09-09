da1e80f8f69037ba1b0728dba4966e6a2594a8a1
calmzhu <saint@justcalm.org>
2019-09-09 08:55:16 +0800

    新增邮件通知

--- chase.py
+++ chase.py
@@ -9,66 +9,80 @@ GTO_ID = "G22CY890"
 XIAOBO_ID = "2Q9GC89GP"
 
 PLAYER_API = "https://royaleapi.com/player/"
-TRUBE_API = "https://royaleapi.com/clan/"
+CLAN_API = "https://royaleapi.com/clan/"
 
 data = [
     {
         "player_id": "2Q9GC89GP",
         "sb_name": "小波",
-        "clan": "", "clan_qq": "", "clan_weixin": ""
+        "clan": "", "clan_id": "", "clan_qq": "", "clan_weixin": "", "clan_num": ""
     },
     {
         "player_id": "G22CY890",
         "sb_name": "GTO_Brand",
-        "clan": "", "clan_qq": "", "clan_weixin": ""
+        "clan": "", "clan_id": "", "clan_qq": "", "clan_weixin": "", "clan_num": ""
     }
 ]
 
 
-
-def get_clan_info(clan_id):
-    url = TRUBE_API + clan_id
-    print('clan info api', url)
+def get_clan_info(clan_id, each_user):
+    url = CLAN_API + clan_id
+    # print('clan info api', url)
     html = requests.get(url).content
     soup = BeautifulSoup(html, "html.parser")
     res = soup.find('div', class_="ui attached container sidemargin0 content_container").find_all('div', class_='value')
     for item in res:
         if '/' in item.string:
-            print('current member', item.text)
+            each_user["clan_num"] = item.text
+            # print('current member', item.text)
 
 
-def _fuck_user(player_id, name):
+def _fuck_user(player_id, name, each_user):
     url = PLAYER_API + player_id
-    print('player info api', url)
     html = requests.get(url).content
     soup = BeautifulSoup(html, "html.parser")
-    res = soup.find('div',id='page_content_container').find_all('div', class_='ui header item')
+    res = soup.find('div', id='page_content_container').find_all('div', class_='ui header item')
     for item in res:
         tribe_link = item.find('a')
         if tribe_link:
             link_str = tribe_link['href']
-            tribe_id = link_str.split('/')[2]
-            print('%s clan id is'%name, tribe_id)
-            get_clan_info(tribe_id)
+            clan_id = link_str.split('/')[2]
+            each_user["clan_id"] = clan_id
+            each_user["clan_num"] = get_clan_current_member_num(clan_id, CLAN_API)
+            each_user["clan_qq"] = try_get_qq(clan_id, CLAN_API)
+
 
 def fuck_xiaobo():
     _fuck_user(XIAOBO_ID, 'xiaobo')
 
+
 def fuck_gto_brand():
     _fuck_user(GTO_ID, 'GTO_brand')
 
-def load_data():
+
+def analysis_data():
     for each_user in data:
-        print(each_user["player_id"])
+        _fuck_user(each_user["player_id"], "XXX", each_user)
+
 
 def show_data():
+    message = ""
     for each_user in data:
-        print(each_user["sb_name"] + "（" + each_user["player_id"] + ")")
-        print("当前部落名：" + each_user["clan"] + " 部落QQ群：" + each_user["clan_qq"])
+        message = message + "SB名：" + each_user["sb_name"] + "（" + each_user["player_id"] + ")" + \
+              " 部落名：" + each_user["clan"] + \
+              " 部落ID：" + each_user["clan_id"] + \
+              " 部落QQ群：" + each_user["clan_qq"] + \
+              " 部落人数：" + each_user["clan_num"] + '\n'
+        print("SB名：" + each_user["sb_name"] + "（" + each_user["player_id"] + ")" +
+              " 部落名：" + each_user["clan"] +
+              " 部落ID：" + each_user["clan_id"] +
+              " 部落QQ群：" + each_user["clan_qq"] +
+              " 部落人数：" + each_user["clan_num"])
+    send_mail(message)
 
 
 def main():
-    load_data()
+    analysis_data()
     show_data()
     #get_clan_description('PPPP0JCQ')
     #fuck_xiaobo()
--- tools.py
+++ tools.py
@@ -1,13 +1,18 @@
 #coding=utf-8
 
 import re
+from email.mime.text import MIMEText
+
 import requests
+import smtplib
 from bs4 import BeautifulSoup
 
 
-def try_get_qq(clan_description):
+def try_get_qq(clan_id, clan_api):
     # Try find qq number
     # First find string of qq
+    qq_number = ''
+    clan_description = get_clan_description(clan_id, clan_api)
     string_qq_index = clan_description.lower().find('qq')
     if string_qq_index != -1:
         # print(string_qq_index)
@@ -15,7 +20,6 @@ def try_get_qq(clan_description):
         # 有时候公告会写成：QQ群是XXX，qq字符串后面的XXX会跟着中文或者非数字，不会直接就是QQ号或者微信号，
         # 所以必须从qq字符开始找到第一个数字
         after_qq_string_array = clan_description[string_qq_index:]
-        qq_number = ''
         for each_string in after_qq_string_array:
             if each_string.isnumeric():
                 qq_number = qq_number + each_string
@@ -25,14 +29,68 @@ def try_get_qq(clan_description):
                 # 当前拼接的qq号为：1234567，当前字符串为『空格』，则不应该继续拼接后面的1（0个人现在）
                 if qq_number.isnumeric():
                     break
-        # print(after_qq_string_array)
-        # print(qq_number)
-        return qq_number
+    if qq_number is None:
+        qq_number = "检索不到QQ"
+    if qq_number == '':
+        qq_number = "检索不到QQ"
+    return qq_number
 
 
-def get_clan_description(clan_id, trube_api):
-    clan_url = trube_api + clan_id
+def get_clan_description(clan_id, clan_api):
+    clan_url = clan_api + clan_id
     html = requests.get(clan_url).content
     soup = BeautifulSoup(html, "html.parser")
     res = soup.find('meta', property="og:description")['content']
     return res
+
+
+# 部落总人数肯定是50人固定值
+def get_clan_current_member_num(clan_id, clan_api):
+    clan_url = clan_api + clan_id
+    html = requests.get(clan_url).content
+    soup = BeautifulSoup(html, "html.parser")
+    res = soup.find('div', class_="ui attached container sidemargin0 content_container").find_all('div', class_='value')
+    for item in res:
+        if '/' in item.string:
+            return item.text.split('/')[0]  # 返回数据类似： 37
+
+
+######################################
+# 1. 检索频率30分钟一次
+######################################
+def trigger_mail(current_member_num, has_qq, has_weichat):
+    # 达到以下条件，触发邮件通知
+    # 1：部落人数小于50人（可以根据部落ID手动进入部落，联系首领）
+    # OR
+    # 2：检索到QQ号（根据QQ号，联系首领）
+    # OR
+    # 3：检索到微信号（根据微信号，联系首领）
+    need_mail = False
+    if current_member_num < 50 | has_qq is True | has_weichat is True:
+        need_mail = True
+    return need_mail
+
+
+def send_mail(mail_content):
+    # 第三方 SMTP 服务
+    mail_host = "smtp.163.com"  # SMTP服务器
+    mail_user = "superman139"  # 用户名
+    mail_pass = "fucksb123"  # 授权密码，非登录密码
+
+    sender = "superman139@163.com"
+    receivers = ['335993634@qq.com', '361705773@qq.com']
+
+    title = '干活啦，搞这两SB了'  # 邮件主题
+
+    message = MIMEText(mail_content, 'plain', 'utf-8')  # 内容, 格式, 编码
+    message['From'] = "{}".format(sender)
+    message['To'] = ",".join(receivers)
+    message['Subject'] = title
+
+    try:
+        smtp_object = smtplib.SMTP_SSL(mail_host, 465)  # 启用SSL发信, 端口一般是465
+        smtp_object.login(mail_user, mail_pass)  # 登录验证
+        smtp_object.sendmail(sender, receivers, message.as_string())  # 发送
+        print("mail has been send successfully.")
+    except smtplib.SMTPException as e:
+        print(e)
